2095a7b52caf0a65ad5b9fee5a1e08718e4f9809
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-26 16:18:36 -0500

    feat: init project

---
+++ src/main/java/com/TourXpress/TourXpressApi/TourXpressApiApplication.java
@@ -0,0 +1,13 @@
+package com.TourXpress.TourXpressApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourXpressApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourXpressApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TourXpress/TourXpressApi/TourXpressApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TourXpress.TourXpressApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourXpressApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
