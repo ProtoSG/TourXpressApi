417fa5c329bd098615fbee9f774892ed011ad40d
ProtoSG <diegoalbertosalzar177@gmail.com>
2025-09-27 19:59:56 -0500

    feat: add health controller

---
+++ src/main/java/com/TourXpress/TourXpressApi/controller/HealthController.java
@@ -0,0 +1,21 @@
+package com.TourXpress.TourXpressApi.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+  @GetMapping("/health")
+  public ResponseEntity<?> healthCheck() {
+    Map<String, String> message = new HashMap<>();
+    message.put("message", "Server active");
+    message.put("status", "OK");
+
+    return ResponseEntity.ok(message);
+  }
+}
